d7a77abaf6a896f0debb17af73d7c6925d30e9ca
djamioufadebi <djamioufadebi1@gmail.com>
2025-05-02 09:00:29 +0100

    initialisation du projet

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\Api\AuthController;
+
+
+Route::controller(AuthController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::apiResource("users", UserController::class);
+    Route::apiResource('clients', ClientController::class);
+    Route::apiResource('invoices', InvoiceController::class);
+    Route::get('/invoices/{id}/pdf', [InvoiceController::class, 'generatePdf']);
+    Route::get('/history', [InvoiceController::class, 'history']);
+});
